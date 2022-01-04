db99c28653e9efea620069febd8085f236bed1ab
Aidan Holvik <Aidan.Holvik@gmail.com>
2022-01-04 15:41:55 -0500

    Added some functions and variables to "Layer.h" and added a function definition for setActivationFunction to "Layer.cpp"

--- Layer.cpp
+++ Layer.cpp
@@ -1,1 +1,23 @@
 #include "Layer.h"
+
+Layer::Layer(unsigned int width, void* activationFunction)
+{
+	this->width = width;
+	bias = new double[width];
+	weight = new double* [width];
+	this->activationFunction = activationFunction;
+}
+
+void Layer::setActivationFunction(void* newActivationFunction)
+{
+	if (newActivationFunction != nullptr) // X1
+	{
+		activationFunction = newActivationFunction;	// doesn't replace function if new function pointer is null
+	}
+
+	/*
+		Issues
+		------------------------------
+			X1: Does not check if the new function pointer points to a valid function. Only checks whether the pointer is null.
+	*/
+}
--- Layer.h
+++ Layer.h
@@ -1,23 +1,37 @@
 #ifndef Layer_H
 #define Layer_H
 
-#include "ActivationFunction.h"
-
 class Layer
 {
 	public:
 		// Constructor(s)
-		Layer(unsigned int width);
+		Layer();
+		Layer(unsigned int width, void* activationFunction);
 
 		// Accessors
+		unsigned int getWidth();
+		double* getBiases();
+		//double getBias(unsigned int neuronIndex);
+		double** getLayerWeights();
+		//double* getNeuronWeights(unsigned int neuronIndex);
+		//double getWeight(unsigned int neuronIndex, unsigned int dendriteIndex);
+		void* getActivationFunction();
+
 		// Mutators
+		void setWidth(unsigned int newWidth);
+		void setBiases(double* newBiases);
+		//void setBias(double newBias, unsigned int neuronIndex);
+		void setLayerWeights(double** newWeights);
+		//void setNeuronWeights(double* newWeights, unsigned int neuronIndex);
+		//void setWeight(double newWeight, unsigned int neuronIndex, unsigned int dendriteIndex);
+		void setActivationFunction(void* newActivationFunction);
 
 
 	private:
-		double* bias;				// array containing the bias of each node in the layer
-		double** weight;			// 2D array containing the outbound weights for each node
-		void* activationFunction;	// pointer to the activation function for this layer's nodes
-
+		unsigned int width;			// The number of neurons in the layer
+		double* bias;				// array containing the bias of each neuron in the layer
+		double** weight;			// 2D array containing the outbound weights for each neuron
+		void* activationFunction;	// pointer to the activation function for this layer's neurons
 
 };
 

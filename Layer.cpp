cfca4c716d1fc7f3eb325c789bfcb91d876e4c71
Aidan Holvik <Aidan.Holvik@gmail.com>
2022-01-04 17:03:26 -0500

    Added some member variables to the Layer class to fit my new approach. Finished the new constructor method, but I may rewrite it again in the future. Still figuring out how I will move data between layers.

--- Layer.cpp
+++ Layer.cpp
@@ -1,3 +1,25 @@
 #include "Layer.h"
 
+Layer::Layer(unsigned int newLayerWidth, unsigned int existingLayerWidth)
+{
+	this->width = newLayerWidth;
+	weight = new double* [width];
 
+	// generate 2D array to hold weighted input
+	input = new double* [existingLayerWidth];	// add a row for each neuron in the previous layer
+	for (int i = 0; i < existingLayerWidth; i++)
+	{
+		input[i] = new double[width];	// add a column for each neuron in this layer
+	}
+
+	bias = new double [width];
+	output = new double [width];
+
+	activationFunction = nullptr; // X1
+
+	/*
+		Issues
+		------------------------------
+			X1: Is not a valid pointer to a function. This will cause issues unless a valid reference is given to the object before trying to pass any input to the network.
+	*/
+}
--- Layer.h
+++ Layer.h
@@ -4,12 +4,15 @@
 class Layer
 {
 	public:
+		Layer(unsigned int newLayerWidth, unsigned int existingLayerWidth);
 
 
 	private:
+		double** input;		// A matrix for storing the weighted inputs from the previous layer. Each row represents a neuron from the previous layer, and each column represents a neuron in this layer.
 		unsigned int width;			// The number of neurons in the layer
-		double* bias;				// array containing the bias of each neuron in the layer
 		double** weight;			// 2D array containing the outbound weights for each neuron
+		double* bias;				// array containing the bias of each neuron in the layer
+		double* output;				// An array which stores the results of the activation function for each neuron in the layer.
 		void* activationFunction;	// pointer to the activation function for this layer's neurons
 
 };
